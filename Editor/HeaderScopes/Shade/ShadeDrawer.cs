0067f2e6ea12563ca66a49330ca2269e41b82461
Hum9183 <hum9183@gmail.com>
2026-02-15 16:11:36 +0900

    Shade機能を大幅に簡素化

--- Editor/HeaderScopes/Shade/ShadeDrawer.cs
+++ Editor/HeaderScopes/Shade/ShadeDrawer.cs
@@ -14,84 +14,8 @@ namespace Hum.HumToonCore.Editor.HeaderScopes.Shade
 
         protected override void DrawMain(MaterialEditor materialEditor)
         {
-            ShadeMode shadeMode = HumToonGUIUtils.DoPopup<ShadeMode>(materialEditor, PropContainer.ShadeMode, ShadeStyles.ShadeMode);
-
-            switch (shadeMode)
-            {
-                case ShadeMode.PosAndBlur:
-                    DrawPosAndBlur(materialEditor);
-                    break;
-                case ShadeMode.Ramp:
-                    DrawRamp(materialEditor);
-                    break;
-                default:
-                    throw new NotImplementedException(nameof(shadeMode));
-            }
-
-            DrawControlMap(materialEditor);
-        }
-
-        private void DrawPosAndBlur(MaterialEditor materialEditor)
-        {
-            bool useFirstShade = HumToonGUIUtils.DrawFloatToggleProperty(PropContainer.UseFirstShade, ShadeStyles.UseFirstShade);
-            if (useFirstShade)
-            {
-                using (new EditorGUI.IndentLevelScope())
-                {
-                    materialEditor.TexturePropertySingleLine(ShadeStyles.FirstShadeMap, PropContainer.FirstShadeMap, PropContainer.FirstShadeColor);
-                    materialEditor.ShaderProperty(PropContainer.FirstShadeBorderPos, ShadeStyles.FirstShadeBorderPos);
-                    materialEditor.ShaderProperty(PropContainer.FirstShadeBorderBlur, ShadeStyles.FirstShadeBorderBlur);
-
-                #if false // 影の中に入ったときにおかしくなるため一旦塞ぐ。　TODO: FakeSSSとして改めて実装し直す
-                    bool useExFirstShade = HumToonGUIUtils.DrawFloatToggleProperty(PropContainer.UseExFirstShade, ShadeStyles.UseExFirstShade);
-                    if (useExFirstShade)
-                    {
-                        using (new EditorGUI.IndentLevelScope())
-                        {
-                            materialEditor.ShaderProperty(PropContainer.ExFirstShadeColor, ShadeStyles.ExFirstShadeColor);
-                            materialEditor.ShaderProperty(PropContainer.ExFirstShadeWidth, ShadeStyles.ExFirstShadeWidth);
-                        }
-                    }
-                #endif
-                }
-                HumToonGUIUtils.Space();
-            }
-
-            bool useSecondShade = HumToonGUIUtils.DrawFloatToggleProperty(PropContainer.UseSecondShade, ShadeStyles.UseSecondShade);
-            if (useSecondShade)
-            {
-                using (new EditorGUI.IndentLevelScope())
-                {
-                    materialEditor.TexturePropertySingleLine(ShadeStyles.SecondShadeMap, PropContainer.SecondShadeMap, PropContainer.SecondShadeColor);
-                    materialEditor.ShaderProperty(PropContainer.SecondShadeBorderPos, ShadeStyles.SecondShadeBorderPos);
-                    materialEditor.ShaderProperty(PropContainer.SecondShadeBorderBlur, ShadeStyles.SecondShadeBorderBlur);
-                }
-                HumToonGUIUtils.Space();
-            }
-        }
-
-        private void DrawRamp(MaterialEditor materialEditor)
-        {
-            bool useRampShade = HumToonGUIUtils.DrawFloatToggleProperty(PropContainer.UseRampShade, ShadeStyles.UseRampShade);
-            if (useRampShade)
-            {
-                using (new EditorGUI.IndentLevelScope())
-                {
-                    materialEditor.TexturePropertySingleLine(ShadeStyles.RampShadeMap, PropContainer.RampShadeMap);
-                }
-            }
-        }
-
-        private void DrawControlMap(MaterialEditor materialEditor)
-        {
-            bool useShadeControlMap = HumToonGUIUtils.DrawFloatToggleProperty(PropContainer.UseShadeControlMap, ShadeStyles.UseShadeControlMap);
-            if (useShadeControlMap)
-            {
-                using (new EditorGUI.IndentLevelScope())
-                {
-                    materialEditor.TexturePropertySingleLine(ShadeStyles.ShadeControlMap, PropContainer.ShadeControlMap, PropContainer.ShadeControlMapIntensity);
-                }
-            }
+            materialEditor.ShaderProperty(PropContainer.ShadeBorderPos, ShadeStyles.ShadeBorderPos);
+            materialEditor.ShaderProperty(PropContainer.ShadeBorderBlur, ShadeStyles.ShadeBorderBlur);
         }
     }
 }
--- Editor/HeaderScopes/Shade/ShadeKeywords.cs
+++ Editor/HeaderScopes/Shade/ShadeKeywords.cs
@@ -1,6 +1,5 @@
 #pragma warning disable CS0414
 
-using System;
 using System.Reflection;
 using Hum.HumToonCore.Editor.Utils;
 using UnityEngine;
@@ -12,87 +11,14 @@ namespace Hum.HumToonCore.Editor.HeaderScopes.Shade
 {
     public class ShadeKeywords
     {
-        // Shade Mode
-        private bool _HT_SHADE_MODE_POS_AND_BLUR;
-        private bool _HT_SHADE_MODE_RAMP;
-
-        // Pos And Blur
-        private bool _HT_USE_FIRST_SHADE;
-        private bool _HT_USE_FIRST_SHADE_MAP;
-        private bool _HT_USE_EX_FIRST_SHADE;
-        private bool _HT_USE_SECOND_SHADE;
-        private bool _HT_USE_SECOND_SHADE_MAP;
-
-        // Ramp
-        private bool _HT_USE_RAMP_SHADE;
-
-        // Control Map
-        private bool _HT_USE_SHADE_CONTROL_MAP;
-
         public void Setup(Material material)
         {
-            // WARNING: 呼び出し順に依存あり
-            SetupShadeMode(material);
-            SetupControlMap(material);
-        }
-
-        private void SetupShadeMode(Material material)
-        {
-            ShadeMode shadeMode = material.GetFloatEnum<ShadeMode>(ID.ShadeMode);
-            switch (shadeMode)
-            {
-                case ShadeMode.PosAndBlur:
-                    _HT_SHADE_MODE_POS_AND_BLUR = true;
-                    SetupPosAndBlur();
-                    break;
-                case ShadeMode.Ramp:
-                    _HT_SHADE_MODE_RAMP = true;
-                    SetupRamp();
-                    break;
-                default:
-                    throw new NotImplementedException(nameof(shadeMode));
-            }
-
-            return;
-
-            void SetupPosAndBlur()
-            {
-                _HT_USE_FIRST_SHADE = material.GetFloat(ID.UseFirstShade).ToBool();
-                _HT_USE_FIRST_SHADE_MAP = material.GetTexture(ID.FirstShadeMap) is not null;
-            #if false // 影の中に入ったときにおかしくなるため一旦塞ぐ。　TODO: FakeSSSとして改めて実装し直す
-                _HT_USE_EX_FIRST_SHADE = material.GetFloat(ID.UseExFirstShade).ToBool() && _HT_USE_FIRST_SHADE;
-            #else
-                _HT_USE_EX_FIRST_SHADE = false;
-            #endif
-                _HT_USE_SECOND_SHADE = material.GetFloat(ID.UseSecondShade).ToBool();
-                _HT_USE_SECOND_SHADE_MAP = material.GetTexture(ID.SecondShadeMap) is not null;
-            }
-
-            void SetupRamp()
-            {
-                bool useRampShade = material.GetFloat(ID.UseRampShade).ToBool();
-                bool rampShadeMapExists = material.GetTexture(ID.RampShadeMap) is not null;
-                _HT_USE_RAMP_SHADE = useRampShade && rampShadeMapExists;
-            }
-        }
-
-        private void SetupControlMap(Material material)
-        {
-            bool useShadeControlMap = material.GetFloat(ID.UseShadeControlMap).ToBool();
-            bool shadeControlMapExists = material.GetTexture(ID.ShadeControlMap) is not null;
-            bool isAtLeastOneShadeValid = _HT_USE_FIRST_SHADE || _HT_USE_SECOND_SHADE || _HT_USE_RAMP_SHADE;
-            _HT_USE_SHADE_CONTROL_MAP = useShadeControlMap && shadeControlMapExists && isAtLeastOneShadeValid;
+            // No keywords to set
         }
 
         public void SetToMaterial(Material material)
         {
-            var fieldInfos = typeof(ShadeKeywords).GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
-            foreach (var fieldInfo in fieldInfos)
-            {
-                string keywordName = fieldInfo.Name;
-                bool state = (bool)fieldInfo.GetValue(this);
-                CoreUtils.SetKeyword(material, keywordName, state);
-            }
+            // No keywords to set
         }
     }
 
